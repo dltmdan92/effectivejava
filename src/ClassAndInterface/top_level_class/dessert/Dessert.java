c4451567f5c4ee4f36e9819eedd5e32ec35b821a
dltmdan92 <dltmdan92@naver.com>
2021-10-25 21:53:22 +0900

    소스 파일 하나에는 톱레벨 클래스를 하나만 담자. 톱레벨이 아니면 멤버 클래스로 선언해라

---
+++ src/ClassAndInterface/top_level_class/dessert/Dessert.java
@@ -0,0 +1,10 @@
+package ClassAndInterface.top_level_class.dessert;
+
+class Dessert {
+    static final String NAME = "pie";
+}
+
+class Utensil {
+    static final String NAME = "pot";
+
+}
---
+++ src/ClassAndInterface/top_level_class/utensil/Utensil.java
@@ -0,0 +1,9 @@
+package ClassAndInterface.top_level_class.utensil;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
