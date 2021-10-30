afcdbbc1ba254346f76f26f2b5eeb9dcac31a9ae
dltmdan92 <dltmdan92@naver.com>
2021-10-30 20:52:10 +0900

    상수 클래스 말고 열거 타입 쓰자.

---
+++ src/generic/enumeration/Operation.java
@@ -0,0 +1,56 @@
+package generic.enumeration;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+/**
+ * 열거 타입을 써라
+ * 정수 상수보다 훨씬 뛰어나고, 강력하고, 안전하다.
+ */
+public enum Operation {
+
+
+    PLUS("+")    {public double apply(double x, double y){return x + y;}}, // 상수별 메서드 구현
+    MINUS("-")   {public double apply(double x, double y){return x - y;}},
+    TIMES("*")   {public double apply(double x, double y){return x * y;}},
+    DIVIDE("/")  {public double apply(double x, double y){return x / y;}};
+
+
+    private final String symbol;
+
+    // enum 타입 생성자가 실행될 때, 접근할 수 있는 필드는 자기 자신의 상수 변수 뿐이다.
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+
+    public abstract double apply(double x, double y);
+
+
+    // enum 타입 생성자가 실행되기 전에는 아직 static 요소들은 초기화 되기 전.
+    public static Optional<Operation> fromString(String symbol) {
+        return Optional.ofNullable(stringToEnum.get(symbol));
+    }
+
+    // 기존 열거 타입에 상수별 동작을 넣을 때는 switch 문이 좋은 선택이 될 수 있다.
+    public static Operation inverse(Operation op) {
+        switch (op) {
+            case PLUS:      return Operation.MINUS;
+            case MINUS:     return Operation.PLUS;
+            case TIMES:     return Operation.DIVIDE;
+            case DIVIDE:    return Operation.TIMES;
+
+            default:    throw new AssertionError("알 수 없는 연산: " + op);
+        }
+    }
+
+    private static final Map<String, Operation> stringToEnum =
+            Stream.of(values()).collect(toMap(Object::toString, e -> e));
+}
---
+++ src/generic/enumeration/PayrollDay.java
@@ -0,0 +1,53 @@
+package generic.enumeration;
+
+import static generic.enumeration.PayrollDay.PayType.WEEKDAY;
+import static generic.enumeration.PayrollDay.PayType.WEEKEND;
+
+/**
+ * 전략 열거 타입 패턴
+ * 내부 열거 타입을 통해 상수별 전략을 부여할 수 있다.
+ * switch 문을 쓰는 것 보다 좋다.
+ */
+public enum PayrollDay {
+    MONDAY(WEEKDAY),
+    TUESDAY(WEEKDAY),
+    WEDNESDAY(WEEKDAY),
+    THURSDAY(WEEKDAY),
+    FRIDAY(WEEKDAY),
+    SATURDAY(WEEKEND),
+    SUNDAY(WEEKEND);
+
+    private final PayType payType;
+
+    PayrollDay(PayType payType) {
+        this.payType = payType;
+    }
+
+    int pay(int minutesWorked, int payRate) {
+        return payType.pay(minutesWorked, payRate);
+    }
+
+    enum PayType {
+        WEEKDAY {
+            @Override
+            int overtimePay(int minsWorked, int payRate) {
+                return minsWorked <= MINS_PER_SHIFT ? 0 :
+                        (minsWorked - MINS_PER_SHIFT) * payRate / 2;
+            }
+        },
+        WEEKEND {
+            @Override
+            int overtimePay(int minsWorked, int payRate) {
+                return minsWorked * payRate / 2;
+            }
+        };
+
+        abstract int overtimePay(int minsWorked, int payRate);
+        private static final int MINS_PER_SHIFT = 8 * 60;
+
+        int pay(int minsWorked, int payRate) {
+            int basePay = minsWorked * payRate;
+            return basePay + overtimePay(minsWorked, payRate);
+        }
+    }
+}
